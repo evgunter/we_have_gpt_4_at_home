f31d859d0045f7fb1e040295357828022f9fb939
Evan Ryan Gunter <evgunter@gmail.com>
2023-05-05 13:50:54 -0700

    working telegram bot which calls out to gpt 4. in-progress terraform server script

---
+++ gpt_4_telegram.py
@@ -0,0 +1,119 @@
+import os
+import dotenv
+import logging
+import telegram.ext
+import time
+import openai
+
+
+# === Setup ============================================================================================================
+
+MODEL = "gpt-4"  # "gpt-3.5-turbo"  # "text-davinci-003"
+LONG_MODEL = "gpt-4-32k"
+FAST_MODEL = "gpt-3.5-turbo"
+
+SYSTEM_PROMPT = {"role": "system", "content": "You are a helpful Telegram chatbot. You can use Telegram message formatting, like `code`, ```code block```, ||spoiler||, **bold**, and __italics__."}
+
+TURBO_COMMAND = 'turbo'
+
+class Message:
+    def __init__(self, message_id, message_text):
+        self.message_id = message_id
+        self.message_text = message_text
+
+# https://github.com/python-telegram-bot/python-telegram-bot/wiki/Extensions-%E2%80%93-Your-first-Bot
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+conversations = {}  # current conversations for each chat
+
+# === Telegram stuff ===================================================================================================
+
+async def start(update: telegram.Update, context: telegram.ext.ContextTypes.DEFAULT_TYPE):
+    logging.debug("adding new chat")
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                      text="welcome!",
+     )  # reply_to_message_id=update.message.message_id)
+
+def reset_conversation(chat_id):
+    conversations[chat_id] = [SYSTEM_PROMPT]  # reset conversation
+
+async def new_conversation(update: telegram.Update, context: telegram.ext.ContextTypes.DEFAULT_TYPE):
+    reset_conversation(update.effective_chat.id)
+    logging.debug("new conversation started")
+    await context.bot.send_message(chat_id=update.effective_chat.id,
+                                   text="new conversation started",
+    )  # reply_to_message_id=update.message.message_id)
+
+async def regular_message(update: telegram.Update, context: telegram.ext.ContextTypes.DEFAULT_TYPE, process_msg=lambda msg: msg, model=MODEL):
+    logging.info(f"update received at {time.time()}: {update}")
+    
+    current_chat_id = update.effective_chat.id
+
+    if current_chat_id not in conversations:
+        reset_conversation(update.effective_chat.id)
+        logging.info("chat missing!")
+        await context.bot.send_message(chat_id=current_chat_id,
+                                       text="looks like i've lost the previous conversation! i've started a new one. send your message again, including any context from the previous conversation.",
+        )  # reply_to_message_id=update.message.message_id)
+        return
+    
+    conversations[current_chat_id].append({"role": "user", "content": process_msg(update.message.text)})
+    logging.info(f"conversations: {conversations}")
+    try:
+        response = query_model(conversations[current_chat_id], model=model)
+    except Exception as e:
+        # TODO: if the model fails for length, use the long context model.
+        #   also, should maybe give some option to remove the last response so it doesn't ruin the whole conversaion,
+        #   if that turns out to be a problem
+        logging.error(e)
+        await context.bot.send_message(chat_id=current_chat_id,
+                                        text=f"model failed with error: {e}",
+        )  # reply_to_message_id=update.message.message_id)
+        return
+    conversations[current_chat_id].append({"role": "assistant", "content": response})
+
+    await context.bot.send_message(chat_id=current_chat_id,
+                                   text=response,
+    )  # reply_to_message_id=update.message.message_id)
+
+async def turbo_message(update: telegram.Update, context: telegram.ext.ContextTypes.DEFAULT_TYPE):
+    logging.info("turbo message received")
+    def removeturbo(msg):
+        return msg[len(TURBO_COMMAND) + 2:]
+
+    await regular_message(update, context, process_msg=removeturbo, model=FAST_MODEL)
+
+
+# === OpenAI stuff =====================================================================================================
+
+def query_model(previous_messages, model=MODEL):
+    response = openai.ChatCompletion.create(model=model, messages=previous_messages)
+    logging.info(f"got response: {response}")
+    return response['choices'][0]['message']['content']
+
+
+if __name__ == '__main__':
+    dotenv.load_dotenv()
+
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+    application = telegram.ext.ApplicationBuilder().token(BOT_TOKEN).build()
+
+    start_handler = telegram.ext.CommandHandler('start', start)
+    new_conversation_handler = telegram.ext.CommandHandler('new_conversation', new_conversation)
+    turbo_handler = telegram.ext.CommandHandler(TURBO_COMMAND, turbo_message)
+
+    regular_message_handler = telegram.ext.MessageHandler(telegram.ext.filters.TEXT & (~telegram.ext.filters.COMMAND),
+                                                          regular_message)
+
+    application.add_handler(start_handler)
+    application.add_handler(regular_message_handler)
+    application.add_handler(new_conversation_handler)
+    application.add_handler(turbo_handler)
+
+    application.run_polling()
---
+++ server.tf
@@ -0,0 +1,55 @@
+provider "google" {
+    project     = "bot-server-383318"
+    region      = "us-west1"
+    zone        = "us-west1-a"
+}
+
+locals {
+    script_directory = "/home"
+    script_name = "bot_script.py"
+}
+
+# SSH in with `gcloud compute ssh bot-server --zone us-west1-a`
+resource "google_compute_instance" "default" {
+    name = "bot-server"
+    machine_type = "f1-micro"
+    zone = "us-west1-a"
+
+    # Debug with `google_metadata_script_runner startup`
+    metadata_startup_script = <<-EOF
+      #!/bin/bash
+
+      # Move to directory containing script created by the file provisioner
+      cd ${local.script_directory}
+
+      # Install required packages for Python script
+      sudo apt update
+      sudo apt -y install python3 python3-pip
+
+      # Set up the cron job to execute the Python script every 4 hours
+      (crontab -l 2>/dev/null; echo "0 */4 * * * cd ${local.script_directory} && python3 ${local.script_name}") | crontab -
+      chmod 600 /var/spool/cron/crontabs/$USER
+  EOF
+
+    provisioner "file" {
+        source = "gpt_4_telegram.py"
+        destination = "${local.script_directory}/${local.script_name}"
+        connection {
+            type = "ssh"
+            user = "google_compute_engine"
+            private_key = file("~/.ssh/google_compute_engine")
+            host = self.network_interface[0].access_config[0].nat_ip
+        }
+    }
+
+    boot_disk {
+        initialize_params {
+            image = "debian-cloud/debian-11"
+        }
+    }
+    network_interface {
+        network = "default"
+
+        access_config {}
+    }
+}
